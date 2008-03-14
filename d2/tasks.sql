e7f1abceb0d2c6153e13a3ef0d527554ff6c6784
mhluongo <mhluongo@8b0068c6-9545-0410-a11e-e7812c790067>
2008-03-14 13:58:48 +0000

    Now I just need to finish the reports.
    - Matt

--- d2/tasks.sql
+++ d2/tasks.sql
@@ -184,7 +184,16 @@
                   '<minTestScore>', '<email>', '<phone>', '<fax>',
                   '<numPositions>');
 -- Search for applicants
-
+  SELECT  NAME,
+          HIGHEST_DEGREE,
+          BIRTH_YEAR,
+          YEARS_EXPERIENCE,
+          CITIZENSHIP
+    FROM  APPLICANT
+   WHERE  HIGHEST_DEGREE>='<highestDegree>'
+          AND BIRTH_YEAR BETWEEN DATE('<lowerYear>') AND DATE('<upperYear>')
+          AND YEARS_EXPERIENCE >= '<yearsExperience>'
+          AND ('<citizenship>' IS NULL OR CITIZENSHIP='<citizenship>');
 -- Show applicant's detail
   SELECT  NAME,
           EMAIL,
@@ -203,6 +212,17 @@
           AND PASSWORD = '<password>';
 
 -- Show report by industry
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- Show report by salary
