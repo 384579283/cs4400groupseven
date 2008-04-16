d87c1c7f851bada0d00d35e4599cc258b40af7ff
ch.martin <ch.martin@8b0068c6-9545-0410-a11e-e7812c790067>
2008-04-16 16:54:06 +0000

    got applicant login working

--- trunk/www/applicant_login.php
+++ trunk/www/applicant_login.php
@@ -1,3 +1,21 @@
+<?
+
+require_once('functions.php');
+
+require_once('db.php');
+
+if (register_post_keys('email', 'password')) {
+    $user_id = $db->applicant_login($email, $password)
+    if ($user_id) {
+        $_SESSION['applicant'] = true;
+        $_SESSION['user_id'] = $user_id;
+        redirect('job_search.php');
+    } else {
+        $error = 'Login failed';
+    }
+}
+
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
   <head>
@@ -6,6 +24,7 @@
     <link rel="stylesheet" href="login.css" type="text/css"/>
   </head>
   <body><form action="applicant_login.php" method="POST">
+    <? include('error.php'); ?>
     <h1>
       Applicant Sign In
     </h1>
@@ -15,7 +34,7 @@
           Email:
         </td>
         <td>
-          <input type="text" class="loginEmailField" />
+          <input type="text" class="loginEmailField" name="email" />
         </td>
       </tr>
       <tr>
@@ -23,7 +42,7 @@
           Password:
         </td>
         <td>
-          <input type="password" class="loginPasswordField" />
+          <input type="password" class="loginPasswordField" name="password" />
         </td>
       </tr>
       <tr>
